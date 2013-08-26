6baf281a136935762c1d8ff2dfbc559dd3b9e46f
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-08-26 00:59:36 -0700

    Add configuration attributes.

--- lib/lita/handlers/travis.rb
+++ lib/lita/handlers/travis.rb
@@ -3,6 +3,11 @@ require "lita"
 module Lita
   module Handlers
     class Travis < Handler
+      def self.default_config(config)
+        config.token = nil
+        config.repos = {}
+      end
+
       http.post "/travis", :receive
     end
 
--- spec/lita/handlers/travis_spec.rb
+++ spec/lita/handlers/travis_spec.rb
@@ -2,4 +2,14 @@ require "spec_helper"
 
 describe Lita::Handlers::Travis, lita_handler: true do
   it { routes_http(:post, "/travis").to(:receive) }
+
+  describe ".default_config" do
+    it "sets token to nil" do
+      expect(Lita.config.handlers.travis.token).to be_nil
+    end
+
+    it "sets repos to an empty hash" do
+      expect(Lita.config.handlers.travis.repos).to eq({})
+    end
+  end
 end
